36ba827eaec4b316e1a58c416f3e4ff6f93fe182
302620548 <302620548@student.rocmondriaan.nl>
2023-02-10 14:39:29 +0100

    progress to form

--- src/Controller/pizzaController.php
+++ src/Controller/pizzaController.php
@@ -1,8 +1,17 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Task;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+
+
 use App\Repository\KlantRepository;
 use App\Repository\PizzaRepository;
+use App\Repository\BestelregelRepository;
 use App\Repository\BestellingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,16 +22,41 @@ class pizzaController  extends AbstractController
     /**
      * @Route("/home")
      */
-    public function home(PizzaRepository $pizzaRepository, KlantRepository $klantRepository, BestellingRepository $bestellingRepository): Response
+    public function home(PizzaRepository $pizzaRepository, KlantRepository $klantRepository, BestellingRepository $bestellingRepository, BestelregelRepository $bestelregelRepository): Response
     {
         $pizza = $pizzaRepository->findAll();
         $klant = $klantRepository->findAll();
         $bestelling = $bestellingRepository->findAll();
+        $bestelregel = $bestelregelRepository->findAll();
 
         return $this->render('pizza/home.html.twig', [
             'pizzas' => $pizza,
             'klanten' => $klant,
-            'bestellingen' => $bestelling
+            'bestellingen' => $bestelling,
+            'bestelregels' => $bestelregel
+        ]);
+    }
+
+    /**
+     * @Route("/form")
+     */
+    public function form(PizzaRepository $pizzaRepository, KlantRepository $klantRepository, BestellingRepository $bestellingRepository, BestelregelRepository $bestelregelRepository): Response
+    {
+
+        $Category = new Category();
+        
+
+
+        $pizza = $pizzaRepository->findAll();
+        $klant = $klantRepository->findAll();
+        $bestelling = $bestellingRepository->findAll();
+        $bestelregel = $bestelregelRepository->findAll();
+
+        return $this->render('pizza/home.html.twig', [
+            'pizzas' => $pizza,
+            'klanten' => $klant,
+            'bestellingen' => $bestelling,
+            'bestelregels' => $bestelregel
         ]);
     }
 }
