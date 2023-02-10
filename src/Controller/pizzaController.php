15eb958e030939eee010a8997c2d8b4e78505e89
302620548 <302620548@student.rocmondriaan.nl>
2023-02-10 12:15:53 +0100

    getting order dated

--- src/Controller/pizzaController.php
+++ src/Controller/pizzaController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Repository\KlantRepository;
 use App\Repository\PizzaRepository;
+use App\Repository\BestellingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +13,16 @@ class pizzaController  extends AbstractController
     /**
      * @Route("/home")
      */
-    public function home(PizzaRepository $pizzaRepository, KlantRepository $klantRepository): Response
+    public function home(PizzaRepository $pizzaRepository, KlantRepository $klantRepository, BestellingRepository $bestellingRepository): Response
     {
         $pizza = $pizzaRepository->findAll();
         $klant = $klantRepository->findAll();
+        $bestelling = $bestellingRepository->findAll();
 
         return $this->render('pizza/home.html.twig', [
             'pizzas' => $pizza,
-            'klanten' => $klant
+            'klanten' => $klant,
+            'bestellingen' => $bestelling
         ]);
     }
 }
